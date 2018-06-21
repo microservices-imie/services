42658a6eaf95db19ca77ada3e16e0da3773faf71
Gwenaël HENOT <gwenael.henot@free.fr>
2018-06-21 14:44:39 +0200

    git subrepo clone (merge) git@github.com:microservices-imie/notifications.git packages/notifications
    subrepo:
      subdir:   "packages/notifications"
      merged:   "d5bcdde"
    upstream:
      origin:   "git@github.com:microservices-imie/notifications.git"
      branch:   "master"
      commit:   "d5bcdde"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

---
+++ packages/notifications/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { RabbitMQReceiver } from '_rabbit-mq/rabbitmq.receiver';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    strategy: new RabbitMQReceiver('amqp://localhost', 'channel'),
+  });
+
+  app.listen(() => ``);
+}
+bootstrap();
---
+++ packages/notifications/src/notifications/tracking.model.ts
@@ -0,0 +1,4 @@
+export class TrackingModel {
+  public name = '';
+  public location = '';
+}
